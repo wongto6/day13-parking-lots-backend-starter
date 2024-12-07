34faa3fab6960dbe652d24025659e078867d8d03
WONGTO6 <wongto6@oocl.com>
2024-12-07 21:23:13 +0800

    feat: update styles

--- parkinglot-frontend/src/component/Ticket.jsx
+++ parkinglot-frontend/src/component/Ticket.jsx
@@ -4,8 +4,10 @@ import React from 'react';
 const Ticket = ({ ticket }) => {
     return (
         <div className="ticket">
-            <p>{ticket.plateNumber}</p>
-            <p className="create-time">{new Date(ticket.createTime).toLocaleString()}</p>
+            <button className="ticket-button">
+                <p>{ticket.plateNumber}</p>
+                <p className="create-time">{new Date(ticket.createTime).toLocaleString()}</p>
+            </button>
         </div>
     );
 };
